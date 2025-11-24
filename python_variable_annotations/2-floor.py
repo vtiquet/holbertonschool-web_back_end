a0a37e1348d8ee4eb5c3a842c5d6bbb91b910c94
vtiquet <valentin.tiquet@gmail.com>
2025-11-24 10:11:07 +0000

    Add type-annotated floor function implementation

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Type-annotated function floor."""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Computes the floor of a floating-point number.
+
+    Args:
+        n: The float number.
+
+    Returns:
+        The floor of the float as an integer.
+    """
+    return math.floor(n)
