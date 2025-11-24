645ac0923389271f19cee67a61bfdc51eb88ea0b
vtiquet <valentin.tiquet@gmail.com>
2025-11-24 10:57:10 +0000

    Add type-annotated zoom_array function for repeating elements in a tuple

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""mypy validation for zoom_array."""
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
+    """
+    Zooms in on an array by repeating each element.
+
+    Args:
+        lst: A tuple of any type elements.
+        factor: The integer factor for zooming.
+
+    Returns:
+        A list containing the zoomed-in elements.
+    """
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+
+zoom_2x = zoom_array(array)
+
+
+# The factor argument must be an int, so we correct it from 3.0 to 3.
+zoom_3x = zoom_array(array, 3)
