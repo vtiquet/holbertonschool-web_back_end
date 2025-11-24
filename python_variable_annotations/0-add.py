889fbae31edb443e90c9f390400e2825198d4257
vtiquet <valentin.tiquet@gmail.com>
2025-11-24 10:01:33 +0000

    Add initial implementation of type-annotated add function and update README files

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Type-annotated function add."""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Args:
+        a: The first float.
+        b: The second float.
+
+    Returns:
+        The sum of the two floats.
+    """
+    return a + b
+
