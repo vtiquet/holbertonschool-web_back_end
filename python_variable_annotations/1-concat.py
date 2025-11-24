8d227a259e13580aa92485a1c9f9651d01e720de
vtiquet <valentin.tiquet@gmail.com>
2025-11-24 10:10:12 +0000

    Change file permissions for add.py and add concat.py implementation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Type-annotated function concat."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1: The first string.
+        str2: The second string.
+
+    Returns:
+        The concatenated string.
+    """
+    return str1 + str2
