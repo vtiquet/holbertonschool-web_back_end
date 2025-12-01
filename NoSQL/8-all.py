32aa7f545346dba3d2e569e216d5e2213d0d155a
vtiquet <valentin.tiquet@gmail.com>
2025-12-01 12:20:58 +0000

    Add function to list all documents in a collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Lists all documents in a collection """
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+    Returns an empty list if no document is found.
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
