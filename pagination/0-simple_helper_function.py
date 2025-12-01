7d491bae2ee35c3e8ff14fa7751632d8f79f74ba
vtiquet <valentin.tiquet@gmail.com>
2025-12-01 09:01:19 +0000

    Refactor docstring in index_range function for improved clarity and formatting

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 """
-Simple helper function to calculate the start and end index for a given page and page size.
+Simple helper function to calculate the start
+and end index for a given page and page size.
 """
 from typing import Tuple
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calculates the start and end index for a given page and page size.
-    Page numbers are 1-indexed.
+    Page numbers are 1-indexed, i.e. the first page is page 1.
+    The function should return a tuple of size two containing a start index
+    and an end index corresponding to the range of indexes to return in a list
+    for those particular pagination parameters.
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
