d1cd28897ac9bccb798ca0c0ed3e28ac6cce778e
vtiquet <valentin.tiquet@gmail.com>
2025-12-08 13:12:19 +0000

    Add Pricing class with constructor, property getters/setters, and methods for price display and conversion

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
