0e70f97790bff0e999ee434976a60442fad91faf
vtiquet <valentin.tiquet@gmail.com>
2025-12-08 14:04:10 +0000

    Add Car class with constructor, property getters/setters, and clone method

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,45 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(newBrand) {
+    if (typeof newBrand !== 'string') {
+      throw new TypeError('Brand must be a string');
+    }
+    this._brand = newBrand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(newMotor) {
+    if (typeof newMotor !== 'string') {
+      throw new TypeError('Motor must be a string');
+    }
+    this._motor = newMotor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor) {
+    if (typeof newColor !== 'string') {
+      throw new TypeError('Color must be a string');
+    }
+    this._color = newColor;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species] || this.constructor;
+    return new Species();
+  }
+}
